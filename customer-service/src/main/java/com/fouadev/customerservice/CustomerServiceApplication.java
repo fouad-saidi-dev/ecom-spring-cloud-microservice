c8edde634cf5142f54b07fdfadfe7738823a6b7a
FOUAD SAIDI <fouadsai17@gmail.com>
2024-11-03 23:21:41 +0100

    first commit

---
+++ customer-service/src/main/java/com/fouadev/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,38 @@
+package com.fouadev.customerservice;
+
+import com.fouadev.customerservice.entities.Customer;
+import com.fouadev.customerservice.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(CustomerRepository customerRepository) {
+        return args -> {
+            customerRepository.save(Customer.builder()
+                    .name("fouad").email("fouad@localhost.com")
+                    .build());
+
+            customerRepository.save(Customer.builder()
+                    .name("reda").email("reda@localhost.com")
+                    .build());
+
+            customerRepository.save(Customer.builder()
+                    .name("mohamed").email("mohamed@localhost.com")
+                    .build());
+
+            customerRepository.findAll().forEach(customer -> {
+                System.out.println(customer.toString());
+                System.out.println("*******************");
+            });
+        };
+    }
+}
