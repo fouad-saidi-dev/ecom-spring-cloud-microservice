fcc5ed6a14dc7af3bff78027a65900d320d94bc7
FOUAD SAIDI <fouadsai17@gmail.com>
2024-11-19 23:04:10 +0100

    config for application client

---
+++ customer-service/src/main/java/com/fouadev/customerservice/config/CorsConfig.java
@@ -0,0 +1,10 @@
+package com.fouadev.customerservice.config;
+/*
+ Created by : Fouad SAIDI on 18/11/2024
+ @author : Fouad SAIDI
+ @date : 18/11/2024
+ @project : project-mircoservice
+*/
+
+public class CorsConfig {
+}
